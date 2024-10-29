38d7a28ddbb32a5e0554f2fb57895e86d12615b4
IlliaCoder <153951865+user14923929@users.noreply.github.com>
2024-10-29 18:51:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let score = 0
+console.warn("Don't use 'score = any_number' or 'for (let index = 0; i < any_number; i++) { addToScore() }', PLASE DONT'T USE IT!!!!!!!!!!")
+
+function addToScore() {
+    if (score >= 100) {
+        showScore()
+    }
+    else {
+        score++
+    }
+}
+
+function showScore() {
+    alert("Score: " + score + "/100")
+}
