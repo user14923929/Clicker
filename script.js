8340576671f0d254f3316c41d86007719b9914f3
IlliaCoder <153951865+user14923929@users.noreply.github.com>
2024-11-12 12:24:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let score = 0
+score = saveScore
 console.warn("Don't use 'score = any_number' or 'for (let index = 0; i < any_number; i++) { addToScore() }', PLASE DONT'T USE IT!!!!!!!!!!")
 
 function addToScore() {
@@ -12,4 +13,15 @@ function addToScore() {
 
 function showScore() {
     alert("Score: " + score + "/5200")
+}
+
+function saveScore() {
+    let savedScore = score
+    document.cookie = saveScore
+    if (document.cookie == score) {
+        alert("Score saved!")
+    }
+    else {
+        alert("ERROR: score not saved, try again later...")
+    }
 }
